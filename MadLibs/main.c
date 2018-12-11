6ac26e0cc2b9d9dcde9e8cc1e7a6b742bd4ea6bf
Sriram <hansolo0210@hotmail.com>
2018-12-10 21:47:26 -0800

    Mad-Libs giraffe academy done

---
+++ MadLibs/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char colors[20];
+printf("Enter a color: ");
+scanf("%s",colors);
+printf("The color you entered is %s",colors);
+int numbers[10];
+numbers[2]=80;
+printf("\n the number is %d",numbers[0]);
+}
